7bbe0c32c2ac4c8acd7064d8b8db6a27e9c8c2f2
esrieger <riegererik0@gmail.com>
2022-09-01 23:16:06 -0400

    Add stats section and correctly answer card

--- html/resources/js/form_handler.js
+++ html/resources/js/form_handler.js
@@ -2,6 +2,8 @@
 let test = 'test1';
 let anki_url = 'http://192.168.0.184:8765'
 
+const deck_array = [];
+
 function testFunction() {
   if (test == 'test1') {
     test = 'test2';
@@ -32,10 +34,85 @@ function postTemplate() {
   }
 }
 
+function getDeckStats() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', anki_url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({
+    'action': 'getDeckStats',
+    'version': 6,
+    'params': { 'decks': deck_array }
+  }));
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      console.log(xhr.responseText);
+      var jsonRsp = JSON.parse(xhr.responseText);
+      if(jsonRsp.error == null) {
+        let deckIds = Object.keys(jsonRsp.result);
+        for (let id = 0; id < deckIds.length; id++) {
+          console.log(deckIds[id]);
+          let deckStats = jsonRsp.result[deckIds[id]];
+          console.log(deckStats);
+          document.getElementById('deck' + id + 'stats').innerHTML = '';
+          document.getElementById('deck' + id + 'stats').innerHTML += '&emsp;&emsp;new_count: ' + deckStats['new_count'] + '<br>';
+          document.getElementById('deck' + id + 'stats').innerHTML += '&emsp;&emsp;learn_count: ' + deckStats['learn_count'] + '<br>';
+          document.getElementById('deck' + id + 'stats').innerHTML += '&emsp;&emsp;review_count: ' + deckStats['review_count'] + '<br>';
+        }
+      }
+    }
+  }
+}
+
 function showAnswer() {
-  var answerButton = document.getElementById('answerButton');
-  document.getElementById('anki_main').removeChild(answerButton);
-  document.getElementById('answer').hidden = false;
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', anki_url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({
+    'action': 'guiShowAnswer',
+    'version': 6,
+  }));
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      console.log(xhr.responseText);
+      var jsonRsp = JSON.parse(xhr.responseText)
+      if(jsonRsp.error == null) {
+        // Remove the show answer button and show the answer
+        var answerButton = document.getElementById('answerButton');
+        document.getElementById('anki_cards').removeChild(answerButton);
+        document.getElementById('answer').hidden = false;
+
+        // remove initial question. Answer card has the question too.
+        var questionPar = document.getElementById('question');
+        document.getElementById('anki_cards').removeChild(questionPar);
+      }
+    }
+  }
+}
+
+function answerCardCb(ease) {
+  var adjustedEase = parseInt(ease) + 1;
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', anki_url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({
+    'action': 'guiAnswerCard',
+    'version': 6,
+    'params': { 'ease': adjustedEase }
+  }));
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      console.log(xhr.responseText);
+      var jsonRsp = JSON.parse(xhr.responseText)
+      if(jsonRsp.error == null) {
+        deckCurrentCard();
+        getDeckStats();
+      }
+    }
+  }
 }
 
 function deckCurrentCard() {
@@ -50,21 +127,42 @@ function deckCurrentCard() {
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       console.log(xhr.responseText);
-      let jsonRsp = JSON.parse(xhr.responseText)
+      let jsonRsp = JSON.parse(xhr.responseText);
       if(jsonRsp.error == null) {
         // Reset anki main section
-        document.getElementById('anki_main').innerHTML = '';
-        let cardKeys = Object.keys(jsonRsp.result)
-        for (let i = 0; i < cardKeys.length; i++) {
-          if (cardKeys[i] == 'question') {
-            document.getElementById('anki_main').innerHTML += jsonRsp.result[cardKeys[i]] + '<br>';
-          } else if(cardKeys[i] == 'answer') {
-            document.getElementById('anki_main').innerHTML += '<div id=answer hidden>' + jsonRsp.result[cardKeys[i]] + '</div>';
+        document.getElementById('anki_cards').innerHTML = '';
+        let cardKeys = Object.keys(jsonRsp.result);
+        for (let cardKey = 0; cardKey < cardKeys.length; cardKey++) {
+          if (cardKeys[cardKey] == 'question') {
+            var questionPar = document.createElement('question');
+            questionPar.setAttribute('id', 'question');
+            questionPar.innerHTML = jsonRsp.result[cardKeys[cardKey]] + '<br><br>';
+            document.getElementById('anki_cards').appendChild(questionPar);
+            //document.getElementById('anki_cards').innerHTML += jsonRsp.result[cardKeys[cardKey]] + '<br>';
+          }
+          else if(cardKeys[cardKey] == 'answer') {
+            document.getElementById('anki_cards').innerHTML += '<div id=answer hidden>' + jsonRsp.result[cardKeys[cardKey]] + '</div>';
             var answerButton = document.createElement('button');
-            answerButton.setAttribute('id', 'answerButton')
-            answerButton.innerHTML = 'Show Answer'
-            answerButton.setAttribute('onclick', 'showAnswer()')
-            document.getElementById('anki_main').appendChild(answerButton);
+            answerButton.setAttribute('id', 'answerButton');
+            answerButton.innerHTML = 'Show Answer';
+            answerButton.setAttribute('onclick', 'showAnswer()');
+            document.getElementById('anki_cards').appendChild(answerButton);
+          }
+          else if (cardKeys[cardKey] == 'nextReviews') {
+            // Create buttons
+            let nextRewiews = jsonRsp.result[cardKeys[cardKey]];
+            let easeKeys = Object.keys(nextRewiews);
+            // add space after Show Answer button
+            document.getElementById('anki_cards').innerHTML += '<br><br>';
+            for (let easeKey = 0; easeKey < easeKeys.length; easeKey++) {
+
+              let easeButton = document.createElement('button');
+              easeButton.setAttribute('id', 'easeButton' + easeKey);
+              easeButton.innerHTML = nextRewiews[easeKeys[easeKey]];
+              easeButton.setAttribute('onclick', 'answerCardCb(\'' + easeKey + '\')');
+              document.getElementById('anki_cards').appendChild(easeButton);
+              document.getElementById('anki_cards').innerHTML += '\t\t\t\t';
+            }
           }
         }
       }
@@ -72,7 +170,7 @@ function deckCurrentCard() {
   }
 }
 
-function deckButtonCb(deckName) {
+function deckButtonCb(deckName, deckId) {
   console.log('deck button press: ' + deckName)
 
   var xhr = new XMLHttpRequest();
@@ -87,17 +185,18 @@ function deckButtonCb(deckName) {
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       console.log(xhr.responseText);
-      var jsonRsp = JSON.parse(xhr.responseText)
+      var jsonRsp = JSON.parse(xhr.responseText);
       if(jsonRsp.error == null) {
+        // Update section header with deck name
         document.getElementById('anki_section_header').innerHTML = '<p>' + deckName + '</p>';
-        document.getElementById('anki_main').innerHTML = '';
 
+        // Update main card with card button
+        document.getElementById('anki_cards').innerHTML = '';
         var cardButton = document.createElement('button');
-        cardButton.setAttribute('id', 'cardButton')
-        cardButton.innerHTML = 'Get Current Card'
-        cardButton.setAttribute('onclick', 'deckCurrentCard()')
-        document.getElementById('anki_main').appendChild(cardButton);
-        console.log('card button')
+        cardButton.setAttribute('id', 'cardButton');
+        cardButton.innerHTML = 'Get Current Card';
+        cardButton.setAttribute('onclick', 'deckCurrentCard()');
+        document.getElementById('anki_cards').appendChild(cardButton);
       }
     }
   }
@@ -116,26 +215,39 @@ function showDecks() {
       console.log(xhr.responseText);
       var jsonRsp = JSON.parse(xhr.responseText)
       if(jsonRsp.error == null) {
-        var rspStr = '';
+        // Reset anki_stats html
+        document.getElementById('anki_stats').innerHTML = '';
 
-        document.getElementById('anki_main').innerHTML = '';
-
-        //console.log('length ' + Object.keys(jsonRsp.result)[1])
         var deckKeys = Object.keys(jsonRsp.result)
+        var deck_ndx = 0;
         for (var i = 0; i < deckKeys.length; i++) {
           if (deckKeys[i] != 'Default') {
             var deckButton = document.createElement('button');
-            deckButton.setAttribute('id', 'deck' + i)
+            deckButton.setAttribute('id', 'deck' + deck_ndx + 'button')
             var btnStr = deckKeys[i]
-            console.log(btnStr)
-            deckButton.innerHTML = btnStr
-            deckButton.setAttribute('onclick', 'deckButtonCb(\'' + btnStr + '\')')
-            document.getElementById('anki_main').appendChild(deckButton);
-            document.getElementById('anki_main').innerHTML += '<br>';
+            console.log(btnStr);
+            var deckId = jsonRsp.result[deckKeys[i]];
+            console.log(deckId);
+            deckButton.innerHTML = btnStr;
+            deckButton.setAttribute('onclick', 'deckButtonCb(\'' + btnStr + '\', \'' + deckId + '\')');
+            document.getElementById('anki_stats').appendChild(deckButton);
+            document.getElementById('anki_stats').innerHTML += '<br>';
+
+            var deckStats = document.createElement('p');
+            deckStats.setAttribute('id', 'deck' + deck_ndx + 'stats');
+            deckStats.innerHTML = '';
+            document.getElementById('anki_stats').appendChild(deckStats);
+
+            deck_array[deck_ndx] = deckKeys[i];
+            deck_ndx += 1;
           }
         }
-      } else {
-        document.getElementById('anki_main').innerHTML = jsonRsp.error;
+
+        // Get deck stats
+        getDeckStats();
+      }
+      else {
+        document.getElementById('anki_stats').innerHTML = jsonRsp.error;
       }
     }
   }
